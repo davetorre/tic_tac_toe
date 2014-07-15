755137bc799567119b3a8cc4e8343e8cf7bd79e9
David Torre <vidtorre@gmail.com>
2014-07-15 14:04:50 -0500

    Refactor rules tests

--- test/tic_tac_toe/rules_test.clj
+++ test/tic_tac_toe/rules_test.clj
@@ -2,41 +2,24 @@
 	(require [tic-tac-toe.board :refer :all]
 					 [tic-tac-toe.rules :refer :all]
 					 [clojure.test      :refer :all]))
-					
+
+(defn board-with-spaces [spaces, player]
+   (reduce #(set-space %1 %2 player) (gen-board) spaces))
+
 (deftest rules-test
 	(testing "[1 1 1] in row has a horizontal winner"
-		(let [board (-> (gen-board)
-		                (set-space 0 1)
-		                (set-space 1 1)
-		                (set-space 2 1))]
-			(is (horizontal-winner? board)))
-		(let [board (-> (gen-board)
-		                (set-space 3 1)
-		                (set-space 4 1)
-		                (set-space 5 1))]
-			(is (horizontal-winner? board)))
-		(let [board (-> (gen-board)
-		                (set-space 6 1)
-		                (set-space 7 1)
-		                (set-space 8 1))]
-			(is (horizontal-winner? board))))
+		(let [first-row-board (board-with-spaces [0 1 2] 1)
+              second-row-board (board-with-spaces [3 4 5] 1)
+		      third-row-board (board-with-spaces [6 7 8] 1)
+              boards [first-row-board second-row-board third-row-board]]
+			(is (every? horizontal-winner? boards))))
 			
 	(testing "[0 0 0] in row has a horizontal winner"
-    	(let [board (-> (gen-board)
-    	                (set-space 0 0)
-    	                (set-space 1 0)
-    	                (set-space 2 0))]
-    		(is (horizontal-winner? board)))
-    	(let [board (-> (gen-board)
-    	                (set-space 3 0)
-    	                (set-space 4 0)
-    	                (set-space 5 0))]
-    		(is (horizontal-winner? board)))
-		(let [board (-> (gen-board)
-    	                (set-space 6 0)
-    	                (set-space 7 0)
-    	                (set-space 8 0))]
-    		(is (horizontal-winner? board))))		
+    	(let [first-row-board (board-with-spaces [0 1 2] 0)
+              second-row-board (board-with-spaces [3 4 5] 0)
+    	      third-row-board (board-with-spaces [6 7 8] 0)
+              boards [first-row-board second-row-board third-row-board]]
+    		(is (every? horizontal-winner? boards))))		
     		
 	(testing "empty board does not have a horizontal winner"
 		(let [board (gen-board)]
@@ -44,52 +27,30 @@
 	
     (testing "[nil 1 nil] in row does not have a horizontal winner"
         ; nor should any heterogeneous row
-		(let [board (-> (gen-board)
-		                (set-space 1 1))]
-			(is (not (horizontal-winner? board))))
-		(let [board (-> (gen-board)
-		                (set-space 4 1))]
-			(is (not (horizontal-winner? board))))
-		(let [board (-> (gen-board)
-		                (set-space 7 1))]
-			(is (not (horizontal-winner? board)))))
+		(let [first-row-board (board-with-spaces [1] 1)
+			  second-row-board (board-with-spaces [4] 1)
+			  third-row-board (board-with-spaces [7] 1)
+			  boards [first-row-board second-row-board third-row-board]]
+			(is (not (every? horizontal-winner? boards)))))
 
     (testing "board has upward diagonal winner"
-        (let [board (-> (gen-board)
-                        (set-space 2 0)
-                        (set-space 4 0)
-                        (set-space 6 0))]
-            (is (diagonal-winner? board)))
-        (let [board (-> (gen-board)
-                        (set-space 2 1)
-                        (set-space 4 1)
-                        (set-space 6 1))]
-            (is (diagonal-winner? board))))
+        (let [upward-zeros (board-with-spaces [2 4 6] 0)
+              upward-ones (board-with-spaces [2 4 6] 1)
+              boards [upward-zeros upward-ones]]
+            (is (every? diagonal-winner? boards))))
             
     (testing "board has downward diagonal winner"
-        (let [board (-> (gen-board)
-                        (set-space 0 0)
-                        (set-space 4 0)
-                        (set-space 8 0))]
-            (is (diagonal-winner? board)))
-        (let [board (-> (gen-board)
-                        (set-space 0 1)
-                        (set-space 4 1)
-                        (set-space 8 1))]
-            (is (diagonal-winner? board))))
+        (let [downward-zeros (board-with-spaces [0 4 8] 0)
+              downward-ones (board-with-spaces [0 4 8] 1)
+              boards [downward-zeros downward-ones]]
+            (is (every? diagonal-winner? boards))))
             
     (testing "game is over with horizontal winner"
-        (let [board (-> (gen-board)
-		                (set-space 0 1)
-		                (set-space 1 1)
-		                (set-space 2 1))]
+        (let [board (board-with-spaces [0 1 2] 1)]
 			(is (game-over? board))))
 			
     (testing "game is over with diagonal winner"
-        (let [board (-> (gen-board)
-		                (set-space 0 0)
-		                (set-space 4 0)
-		                (set-space 8 0))]
+        (let [board (board-with-spaces [0 4 8] 1)]
 			(is (game-over? board))))
 			
     (testing "game is over with full board"
