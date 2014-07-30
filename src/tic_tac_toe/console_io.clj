b9c7dba94ed18d806cd5e2b07137b763afd36445
David Torre <vidtorre@gmail.com>
2014-07-30 14:19:58 -0500

    Create IOProtocol, take println and read-line out of game logic

--- src/tic_tac_toe/console_io.clj
+++ src/tic_tac_toe/console_io.clj
@@ -2,30 +2,41 @@
     (:require [tic-tac-toe.board  :refer :all]
               [tic-tac-toe.rules  :refer :all]
               [tic-tac-toe.player :refer :all]))
-                  
-(defn print-board [board]
+ 
+(defprotocol IOProtocol
+    (io-print [this something])
+    (io-read [this]))
+
+(deftype ConsoleIO []
+    IOProtocol
+    (io-print [this something]
+        (println something))
+    (io-read [this]
+        (read-line)))
+
+(defn prompt [io question]
+    (io-print io question)
+    (io-read io))
+    
+(defn print-board [io board]
     (let [nice-board (replace {nil "-" 0 "X" 1 "O"} board)
           nice-rows (vec (get-rows nice-board))]
 
-        (doall (map println nice-rows))))
-              
-(defn prompt [question]
-    (println question)
-    (read-line))
+        (doall (map #(io-print io %) nice-rows))))
     
-(defn get-human-move [board]
+(defn get-human-move [io board]
     (let [open-spaces (seq (get-open-spaces board))
-          user-input (prompt (str "What's your move? " open-spaces))
+          user-input (prompt io (str "What's your move? " open-spaces))
           open-spaces-strings (map #(str %) open-spaces)]
         
         (if (contains? (set open-spaces-strings) user-input)
             (let [move (Integer/parseInt user-input)
                   token (get-token board)]    
                 (set-space board move token))
-            (get-human-move board))))
+            (get-human-move io board))))
             
-(defn human-goes-first? []
-    (let [answer (prompt "Would you like to go first? (y/n)")
+(defn human-goes-first? [io]
+    (let [answer (prompt io "Would you like to go first? (y/n)")
           yeses #{"y" "Y" "Yes" "YES" "yes"}
           noes  #{"n" "N" "No"  "NO"  "no"}]
           
@@ -34,17 +45,18 @@
             (contains? noes answer) false
             :else (human-goes-first?))))
 
-(defn game-loop [board turn]
+(defn game-loop [io board turn]
     (if (= turn :human)
-        (print-board board))
+        (print-board io board))
 
     (if (game-over? board)
-        (println "Game over. Bye!")
+        (io-print io "Game over. Bye!")
         (if (= turn :human)
-            (game-loop (get-human-move board) :cpu)
-            (game-loop (make-move board) :human))))  
+            (game-loop io (get-human-move io board) :cpu)
+            (game-loop io (make-move board) :human))))  
        
 (defn -main [& args]
-    (if (human-goes-first?)
-        (game-loop (gen-board) :human)
-        (game-loop (gen-board) :cpu)))
+    (let [io (ConsoleIO.)]
+        (if (human-goes-first? io)
+            (game-loop io (gen-board) :human)
+            (game-loop io (gen-board) :cpu))))
--- test/tic_tac_toe/console_io_test.clj
+++ test/tic_tac_toe/console_io_test.clj
@@ -5,54 +5,37 @@
              [tic-tac-toe.console-io :refer :all]
              [clojure.test           :refer :all]))
              
-(defn get-prompt-print-out [question]
-    (with-out-str (with-in-str "--" (prompt question))))
-    
-(defn get-prompt-output [answer]
-    (with-in-str answer (prompt "--")))
-
-(defn make-input-sequence [coll]
-    (apply str (interleave coll (repeat "\n"))))
-                 
+(deftype TestIO [input]
+    IOProtocol
+    (io-print [this something]
+        something)
+    (io-read [this]
+        input))
+                     
 (deftest console-io-test
 
-    (testing "prompt prints out its question argument"
-        (let [questions ["Hello!" "Have you ever transcended space and time?"]
-              questions-w-newline (map #(str % "\n") questions)]
-        
-            (is (= (map #(get-prompt-print-out %) questions) questions-w-newline))))
-
     (testing "prompt outputs the user's answer"
-        (let [answers ["Hi" "5"]]
-        
-            (is (= (map #(get-prompt-output %) answers) answers))))
-        
+        (let [io (TestIO. "Blue")]
+            (is (= (prompt io "What is your favorite color?") "Blue"))))
+            
     (testing "get-human-move marks user's move when going first"
         (let [board (gen-board)
-              board-after (board-with-spaces [0] 0)]
+              board-after (board-with-spaces [0] 0)
+              io (TestIO. "0")]
               
-            (is (= (with-in-str "0" (get-human-move board)) board-after))))
-            
+            (is (= (get-human-move io board) board-after))))
+    
     (testing "get-human-move marks user's move when going second"
         (let [board (board-with-spaces [0] 0)
               board-after (-> (gen-board)
                               (set-space 0 0)
-                              (set-space 4 1))]
+                              (set-space 4 1))
+              io (TestIO. "4")]
           
-            (is (= (with-in-str "4" (get-human-move board)) board-after))))    
-    
-    (testing "get-human-move asks until receiving valid move"
-        (let [board (board-with-spaces [5] 0)
-              input-sequence (make-input-sequence ["5" "not valid" "8"]) 
-              result (set-space board 8 1)]
+            (is (= (get-human-move io board) board-after))))
             
-            (is (= (with-in-str input-sequence (get-human-move board)) result)))) 
-    
     (testing "human-goes-first? returns true if user responds Y"
-        (is (with-in-str "Y" (human-goes-first?))))
+        (is (human-goes-first? (TestIO. "Y"))))
         
     (testing "human-goes-first? returns false if user responds N"
-        (is (not (with-in-str "N" (human-goes-first?)))))
-            
-    (testing "human-goes-first? asks until receiving valid answer"
-        (is (with-in-str "nobody\n27\nY" (human-goes-first?)))))
+        (is (not (human-goes-first? (TestIO. "N"))))))
