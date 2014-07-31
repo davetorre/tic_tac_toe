d1770f2dbf681e36a2d766bd6745396540d62cef
David Torre <vidtorre@gmail.com>
2014-07-31 11:26:40 -0500

    Refactor MinMaxPlayer tests

--- test/tic_tac_toe/player_test.clj
+++ test/tic_tac_toe/player_test.clj
@@ -5,11 +5,14 @@
              [tic-tac-toe.console-io :refer :all]
              [clojure.test           :refer :all]))
 
+(defn make-minmax-move [board]
+    (make-move (new-minmax-player) (new-console-io) board))
+
 (defn play-minmax-game [board]
     (loop [board board]
         (if (game-over? board)
             board
-            (recur (make-move (new-minmax-player) (new-console-io) board)))))
+            (recur (make-minmax-move board)))))
                          
 (deftest player-test
     
@@ -17,20 +20,20 @@
         (let [board (-> (gen-board)
                     (set-spaces [0 1 5 6] 0)
                     (set-spaces [2 3 4 7] 1))
-              result (make-move (new-minmax-player) (new-console-io) board)]
+              result (make-minmax-move board)]
               
             (is (= (- (num-open-spaces board) 1) (num-open-spaces result)))))
 
     (testing "MinMaxPlayer doesn't make move in finished game board"
         (let [board (board-with-spaces [0 1 2] 0)]
         
-            (is (= board (make-move (new-minmax-player) (new-console-io) board)))))
+            (is (= board (make-minmax-move board)))))
     
     (testing "MinMaxPlayer makes move with correct token (X)"
         (let [board (-> (gen-board)
                         (set-spaces [0 1 5 6] 0)
                         (set-spaces [2 3 4 7] 1))
-              result (make-move (new-minmax-player) (new-console-io) board)]
+              result (make-minmax-move board)]
 
             (is (= (+ (occurences board 0) 1) (occurences result 0)))))       
             
@@ -38,7 +41,7 @@
         (let [board (-> (gen-board)
                         (set-spaces [0 1 5 6] 0)
                         (set-spaces [2 3 4] 1))
-              result (make-move (new-minmax-player) (new-console-io) board)]
+              result (make-minmax-move board)]
               
             (is (= (+ (occurences board 1) 1) (occurences result 1)))))
 
@@ -53,4 +56,4 @@
                         (set-spaces [1 3 5] 0)
                         (set-spaces [0 4] 1))]
                         
-            (is (game-over? (make-move (new-minmax-player) (new-console-io) board))))))
+            (is (game-over? (make-minmax-move board))))))
